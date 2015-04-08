561dd71561d801370b62b5556839f78d45f31f7f
Whitney <whitneywooyy@gmail.com>
2015-04-08 15:39:55 -0600

    Completed 3rd problem

--- arrayReview.js
+++ arrayReview.js
@@ -35,13 +35,21 @@ newArr(evenArray);
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+// below you're given a function that will return a random number between 0 and 30 and an array full or numbers 
+// 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to 
+// see if that random number is in the array. If it is, alert true, if it's not, alert false
+
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
+
+ var solution = function(arr) {
+ 	return arr.indexOf(getRandomArbitrary()) >= 0;
+ }; 
+ solution(randomArray);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
