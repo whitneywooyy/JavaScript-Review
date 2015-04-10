14ee8af7384123443675883531ffcc6b2d3d72e8
Whitney <whitneywooyy@gmail.com>
2015-04-09 20:19:36 -0600

    Completed first problem

--- callbackReview.js
+++ callbackReview.js
@@ -1,6 +1,8 @@
 /* Declare and Define the functions here that will make the function calls below work properly */
 
-
+var first = function(arr, cb) {
+  return cb(arr[0]);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
